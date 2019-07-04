93b7fcb05b866e28d6beb4cb753f149ef7883f1a
xiaobo <xiaobo@hangsheng.com.cn>
2019-07-04 02:37:38 -0700

    add env.sh

---
+++ env.sh
@@ -0,0 +1,12 @@
+# toolchains script
+export PATH=/disk4/tool/arm-2012.09/bin:$PATH
+#export RTT_ROOT=/home/coding/workspace/rt-thread
+export RTT_CC=gcc
+export RTT_EXEC_PATH=/disk4/tool/arm-2012.09/bin
+
+# export KDIR=/home/coding/workspace/linux-3.18.16
+#export KDIR=/home/coding/workspace/buildroot-2015.05/output/build/linux-custom
+export PATH=/disk4/tool/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabi/bin:$PATH
+export CROSS_COMPILE=arm-linux-gnueabi-
+export ARCH=arm
+#export MO_DIR=/home/coding/workspace/buildroot-2015.05/output/target/root
